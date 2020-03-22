280a87e3b0834e23324cb8c55f535b0250cb7bb4
Gal Ben David <gal@intsights.com>
2020-03-23 00:18:39 +0200

    bumped to version 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 setuptools.setup(
     name='PyRepScan',
-    version='0.2.1',
+    version='0.2.2',
     author='Gal Ben David',
     author_email='gal@intsights.com',
     url='https://github.com/intsights/PyRepScan',
