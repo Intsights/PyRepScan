d4a170bdf4e6d323d6474c71392700d013762ceb
Gal Ben David <gal@intsights.com>
2020-09-01 18:38:35 +0300

    wip

--- setup.py
+++ setup.py
@@ -25,8 +25,12 @@ setuptools.setup(
     keywords='git repository leaks scanner detector libgit2 re2 c++',
     python_requires='>=3.6',
     zip_safe=False,
+    setup_requires=[
+        'pytest-runner',
+    ],
     tests_require=[
         'gitpython',
+        'pytest',
     ],
     package_data={},
     include_package_data=True,
